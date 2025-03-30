b8a513ef2fd71f47266747627549611b081caa65
Богдан Безуглый <winzar06031977@gmail.com>
2025-03-30 23:33:57 +0300

    Complete practical work

--- Practical_work_2_visual_programming/MainWindow.xaml.cs
+++ Practical_work_2_visual_programming/MainWindow.xaml.cs
@@ -13,71 +13,65 @@ using System.Windows.Threading;
 
 namespace Practical_work_2_visual_programming;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
-    private RotateTransform3D myYRotate, myZRotate, myYRotate2, myZRotate2;
-    private AxisAngleRotation3D myYAxis, myZAxis, myYAxis2, myZAxis2;
 
-    private Transform3DGroup myTransform1, myTransform2;
+    private RotateTransform3D myRotate1;
+    private AxisAngleRotation3D myAxis1;
+    private TranslateTransform3D myTranslate1;
+    private Transform3DGroup myTransform1;
+
+    private RotateTransform3D myRotate2;
+    private AxisAngleRotation3D myAxis2;
+    private TranslateTransform3D myTranslate2;
+    private Transform3DGroup myTransform2;
+
+    private RotateTransform3D myRotate3;
+    private AxisAngleRotation3D myAxis3;
+    private TranslateTransform3D myTranslate3;
+    private Transform3DGroup myTransform3;
+
+
     private DispatcherTimer MyTimer;
     private void Window_Loaded(object sender, RoutedEventArgs e)
     {
-        myYRotate = new RotateTransform3D();
-        myYAxis = new AxisAngleRotation3D();
-        myYAxis.Axis = new Vector3D(0, 1, 0);
-        myYAxis.Angle = 0;
-        myYRotate.Rotation = myYAxis;
-
+        myAxis1 = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 12);
+        myRotate1 = new RotateTransform3D(myAxis1);
+        myTranslate1 = new TranslateTransform3D(0, 0, 0);
         myTransform1 = new Transform3DGroup();
-        myModel.Transform = myTransform1;
+        Cube.Transform = myTransform1;
+        myTransform1.Children.Add(myTranslate1);
+        myTransform1.Children.Add(myRotate1);
 
-        myZRotate = new RotateTransform3D();
-        myZAxis = new AxisAngleRotation3D();
-        myZAxis.Axis = new Vector3D(0, 0, 1);
-        myZAxis.Angle = 0;
-        myZRotate.Rotation = myZAxis;
-
-        myTransform1.Children.Add(myYRotate);
-        myTransform1.Children.Add(myZRotate);
-
-        myYRotate2 = new RotateTransform3D();
-        myYAxis2 = new AxisAngleRotation3D();
-        myYAxis2.Axis = new Vector3D(0, 1, 0);
-        myYAxis2.Angle = 0;
-        myYRotate2.Rotation = myYAxis2;
-        myZRotate2 = new RotateTransform3D();
-        myZAxis2 = new AxisAngleRotation3D();
-        myZAxis2.Axis = new Vector3D(0, 0, 1);
-        myZAxis2.Angle = 0;
-        myZRotate2.Rotation = myZAxis2;
+        myAxis2 = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 2);
+        myRotate2 = new RotateTransform3D(myAxis2);
+        myTranslate2 = new TranslateTransform3D(0, 0, 0);
         myTransform2 = new Transform3DGroup();
-        MyModel2.Transform = myTransform2;
+        Pyramid.Transform = myTransform2;
+        myTransform2.Children.Add(myTranslate2);
+        myTransform2.Children.Add(myRotate2);
 
-        myTransform2.Children.Add(myYRotate2);
-        myTransform2.Children.Add(myZRotate2);
+        myAxis3 = new AxisAngleRotation3D(new Vector3D(0, 1, 0), 50);
+        myRotate3 = new RotateTransform3D(myAxis3);
+        myTranslate3 = new TranslateTransform3D(0, 0, 0);
+        myTransform3 = new Transform3DGroup();
+        Triangle.Transform = myTransform3;
+        myTransform3.Children.Add(myTranslate3);
+        myTransform3.Children.Add(myRotate3);
 
         MyTimer = new DispatcherTimer();
         MyTimer.Tick += new EventHandler(My_Timer_tick);
         MyTimer.Interval = new TimeSpan(100000);
-    }
-    private void Button_Click_1(object sender, RoutedEventArgs e)
-    {
-        MyTimer.Stop();
-    }
-
-    private void Button_Click(object sender, RoutedEventArgs e)
-    {
         MyTimer.Start();
     }
     private void My_Timer_tick(object sender, EventArgs e)
     {
-        myYAxis.Angle += 1;
-        myZAxis.Angle += 1;
-        myYAxis2.Angle -= 2;
-        myZAxis2.Angle -= 2;
+        myAxis1.Angle += 0.03;
+        myAxis2.Angle -= 0.04;
+        myAxis3.Angle -= 0.08;
+        myTranslate1.OffsetZ += 0.00005;
+        myTranslate2.OffsetZ += 0.00005;
+        myTranslate3.OffsetZ += 0.00005;
     }
     public MainWindow()
     {
